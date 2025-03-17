c98068f81f6a41ad83d39364d2eec6e63388a314
rogierx <rkmuller1@gmail.com>
2025-03-17 19:54:34 +0100

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false,
+    };
+    return config;
+  },
+}
